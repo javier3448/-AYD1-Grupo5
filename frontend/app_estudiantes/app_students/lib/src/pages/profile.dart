c4afe156e0b7dda1c5c31944d265effca0e0f4c2
Elba Alvarez <elba1995@gmail.com>
2021-02-27 10:05:05 -0600

    Pantalla de Perfil diseñada 201408549

--- frontend/app_estudiantes/app_students/lib/src/pages/profile.dart
+++ frontend/app_estudiantes/app_students/lib/src/pages/profile.dart
@@ -1,22 +1,149 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_session/flutter_session.dart';
-import 'package:app_students/src/pages/session.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+import 'package:app_students/src/pages/login.dart';
 
 class Profile_page extends StatelessWidget {
-  const Profile_page({Key key}) : super(key: key);
+  // const Profile_page({Key key}) : super(key: key);
+  //List DatosUsuario;
+  var DatosUsuario;
+
+  getUser() async {
+    http.Response response =
+        await http.get('http://13.58.126.153:4000/api/login');
+    Map respuesta = json.decode(response.body);
+    DatosUsuario = Usuario.fromJson(respuesta);
+
+    //DatosUsuario = respuesta['usuario'];
+    //respuesta['id'];
+    debugPrint(response.body);
+  }
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Container(
-            width: 200.0,
-            height: 200.0,
-            decoration: BoxDecoration(color: Colors.blue),
-            child: Center(
-              child: LeerSesion(),
-            )),
+  void initState() {
+    //super.initState();
+    getUser();
+  }
+
+  Widget textfield({@required String hintText}) {
+    return Material(
+      elevation: 4,
+      shadowColor: Colors.grey,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+            hintText: hintText,
+            hintStyle: TextStyle(
+              letterSpacing: 2,
+              color: Colors.indigo[700],
+              fontWeight: FontWeight.bold,
+            ),
+            fillColor: Colors.white30,
+            filled: true,
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                borderSide: BorderSide.none)),
       ),
     );
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                height: 450,
+                width: double.infinity,
+                margin: EdgeInsets.symmetric(horizontal: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    textfield(hintText: 'Nombre completo'),
+                    textfield(hintText: 'Email'),
+                    textfield(hintText: 'CUI'),
+                    textfield(hintText: 'Carnet'),
+                  ],
+                ),
+              )
+            ],
+          ),
+          CustomPaint(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+            ),
+            painter: HeaderCurvedContainer(),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: Text(
+                  "Perfil",
+                  style: TextStyle(
+                      fontSize: 35,
+                      letterSpacing: 1.5,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                width: MediaQuery.of(context).size.width / 2,
+                height: MediaQuery.of(context).size.width / 2,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white, width: 5),
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage('lib/src/pages/perfil.jpg'),
+                  ),
+                ),
+              )
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.only(bottom: 270, left: 184),
+            child: CircleAvatar(
+              backgroundColor: Colors.indigo[600],
+              child: IconButton(
+                icon: Icon(
+                  Icons.edit,
+                  color: Colors.white,
+                ),
+                onPressed: () {},
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class HeaderCurvedContainer extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()..color = Color(0xff303f9f);
+    Path path = Path()
+      ..relativeLineTo(0, 150)
+      ..quadraticBezierTo(size.width / 2, 225, size.width, 150)
+      ..relativeLineTo(0, -150)
+      ..close();
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
 }
