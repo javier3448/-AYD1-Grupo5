486c9562cd02a432f14ea3eb24f98ce00cf74cfe
Oscar Mazariegos <oscarmazariegoss5@gmail.com>
2021-03-16 22:37:03 -0600

    Pantalla para ver los cursos asignados de un estudiante 201602469

--- frontend/app_estudiantes/app_students/lib/src/pages/cursos.dart
+++ frontend/app_estudiantes/app_students/lib/src/pages/cursos.dart
@@ -8,12 +8,71 @@ class Cursos extends StatefulWidget {
 }
 
 class _CursosState extends State<Cursos> {
+  Widget MiCurso(codigo, nombre, seccion, hInicio, hFinal) {
+    return ExpansionTile(
+      title: Text(nombre),
+      subtitle: Text("Inicia: " + hInicio + "  -  Finaliza:" + hFinal),
+      leading: CircleAvatar(
+        child: Text(codigo),
+        backgroundColor: Color.fromRGBO(15, 40, 80, 1),
+      ),
+      trailing: Icon(Icons.expand_more_rounded),
+      children: [
+        Column(
+          children: [
+            Text(
+              codigo + " - " + nombre,
+              style: TextStyle(
+                  fontSize: 23.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+            Text(
+              "Seccion: " + seccion,
+              style: TextStyle(fontSize: 20.0, color: Colors.white),
+            ),
+            Divider(height: 20.0)
+          ],
+        )
+      ],
+      backgroundColor: Color.fromRGBO(15, 40, 80, 1),
+      initiallyExpanded: false,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Container(
-          child: Text("Mis Cursos actualizar"),
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      appBar: null,
+      body: Container(
+        child: Center(
+          child: Container(
+            child: ListView(
+              children: [
+                Container(
+                  child: Text(
+                    "MIS CURSOS",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25.0,
+                        fontStyle: FontStyle.italic),
+                  ),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      color: Color.fromRGBO(15, 40, 80, 1)),
+                  margin: EdgeInsets.all(25.0),
+                  padding: EdgeInsets.all(40.0),
+                ),
+                MiCurso("275", "Progra 1", "A-", "12:00", "1:40"),
+                MiCurso("275", "Progra 1", "A-", "12:00", "1:40"),
+                MiCurso("275", "Progra 1", "A-", "12:00", "1:40"),
+                MiCurso("275", "Progra 1", "A-", "12:00", "1:40"),
+                MiCurso("275", "Progra 1", "A-", "12:00", "1:40"),
+              ],
+            ),
+          ),
         ),
       ),
     );
