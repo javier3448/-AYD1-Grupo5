77d6af555a821c559c8a8acb0034cbd61ce8a264
Oscar Mazariegos <oscarmazariegoss5@gmail.com>
2021-03-30 02:13:27 -0600

    Dashboard para administrador 201602469

--- frontend/app_estudiantes/app_students/lib/src/pages/admin_home.dart
+++ frontend/app_estudiantes/app_students/lib/src/pages/admin_home.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class Admin_page extends StatefulWidget {
@@ -8,14 +10,101 @@ class Admin_page extends StatefulWidget {
 }
 
 class _Admin_pageState extends State<Admin_page> {
+  var imgE = Image.network(
+    "https://i.ibb.co/12x67R8/estudiantes.png",
+    height: 150.0,
+  );
+  var imgL = Image.network(
+    "https://i.ibb.co/3FwR6fM/libro.png",
+    height: 150.0,
+  );
+  var imgQ = Image.network(
+    "https://i.ibb.co/GQgYp7M/libro-abierto.png",
+    height: 150.0,
+  );
+
+  Widget cantidad(cantidad) {
+    return Text(
+      cantidad,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+          color: Colors.white, fontSize: 100, fontWeight: FontWeight.bold),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Text(
-          "ADMIN HOME",
-          style: TextStyle(fontSize: 30.0),
-        ),
+      color: Colors.blueAccent,
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              "Dashboard",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 35.0,
+                  fontWeight: FontWeight.w400,
+                  decoration: TextDecoration.none),
+            ),
+          ),
+          Divider(
+            height: 3,
+            thickness: 2,
+            indent: 75,
+            endIndent: 75,
+          ),
+          Row(
+            children: [
+              Expanded(
+                  flex: 1,
+                  child: Column(
+                    children: [
+                      imgE,
+                      Text(
+                        "Estudiantes Registrados",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w400,
+                            decoration: TextDecoration.none),
+                      )
+                    ],
+                  )),
+              Expanded(flex: 1, child: cantidad("000")),
+            ],
+          ),
+          Divider(
+            height: 50,
+            thickness: 5,
+            indent: 25,
+            endIndent: 25,
+          ),
+          Row(
+            children: [
+              Expanded(flex: 1, child: cantidad("00")),
+              Expanded(
+                  flex: 1,
+                  child: Column(
+                    children: [
+                      imgL,
+                      Text(
+                        "Cursos Existentes",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w400,
+                            decoration: TextDecoration.none),
+                      )
+                    ],
+                  ))
+            ],
+          )
+        ],
       ),
     );
   }
