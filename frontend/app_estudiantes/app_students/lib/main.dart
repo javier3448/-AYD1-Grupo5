71eeec95229dc8989dc5fb3b54cd9cdba6de4365
javier <javier3448.ja@gmail.com>
2021-04-07 15:05:53 -0600

    Agregar bontones e imagen a cada estudiante en la pagina estudiantes 20612383
    Se utilizaron futures para que no fuera tan incomoda la espera por la
    respuesta de NetworkImage.

--- frontend/app_estudiantes/app_students/lib/main.dart
+++ frontend/app_estudiantes/app_students/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Students App',
-      initialRoute: 'login',
+      initialRoute: 'estudiantes',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
           primaryColor: Color.fromRGBO(15, 45, 80, 1),
